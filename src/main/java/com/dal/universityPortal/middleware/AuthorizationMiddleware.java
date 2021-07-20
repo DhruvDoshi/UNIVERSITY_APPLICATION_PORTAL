7aa1ab710dab16cc3c564cbb5c8074803b149de8
Aasif Faizal <as826032@dal.ca>
2021-07-20 14:41:37 +0530

    Added error pages

--- src/main/java/com/dal/universityPortal/middleware/AuthorizationMiddleware.java
+++ src/main/java/com/dal/universityPortal/middleware/AuthorizationMiddleware.java
@@ -6,13 +6,19 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 public class AuthorizationMiddleware implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
-        return user.getTypeEnum().isRouteAllowed(request.getRequestURI());
+        boolean isAuthorized = user.getTypeEnum().isRouteAllowed(request.getRequestURI());
+        if (isAuthorized) {
+            return true;
+        }
+        response.sendRedirect("/error/unauthorized");
+        return false;
     }
 }
