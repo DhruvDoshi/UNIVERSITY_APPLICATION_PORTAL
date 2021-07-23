7fdf48c8abccff99042433ab1366282184911834
Foram Prashant Gaikwad <fr700535@dal.ca>
2021-07-23 11:07:32 -0300

    review_application version 1

---
+++ src/main/java/com/dal/universityPortal/service/ReviewApplicationServiceImpl.java
@@ -0,0 +1,35 @@
+package com.dal.universityPortal.service;
+
+import com.dal.universityPortal.database.ReviewApplicationDao;
+import com.dal.universityPortal.model.Application;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class ReviewApplicationServiceImpl implements ReviewApplicationService{
+
+    ReviewApplicationDao reviewApplicationDao = new ReviewApplicationDao();
+
+    @Override
+    public List<Application> readList() throws SQLException {
+        return reviewApplicationDao.fetchAll();
+    }
+
+    @Override
+    public Application oneApplication(int id) throws SQLException {
+        return reviewApplicationDao.fetchAllByParam(id);
+    }
+
+    @Override
+    public Boolean saveReviewApplication(Application application){
+        try{
+            System.out.println("Lock");
+            reviewApplicationDao.update(application);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return true;
+    }
+}
