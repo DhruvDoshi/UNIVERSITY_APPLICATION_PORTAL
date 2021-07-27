b1c61bcf4f930bff8730d55f3256336a6385bc23
Jaspreet Kaur Gill <js523380@dal.ca>
2021-07-27 11:39:37 -0300

    Revert "Merge branch 'simplifying_for_loop' into 'master'"
    This reverts merge request !51

--- src/main/java/com/dal/universityPortal/service/UniversityProfileServiceImpl.java
+++ src/main/java/com/dal/universityPortal/service/UniversityProfileServiceImpl.java
@@ -1,11 +1,16 @@
 package com.dal.universityPortal.service;
 
+import com.dal.universityPortal.database.DBSession;
+import com.dal.universityPortal.database.ProgramDao;
 import com.dal.universityPortal.database.UniversityDao;
+import com.dal.universityPortal.model.Program;
 import com.dal.universityPortal.model.University;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UniversityProfileServiceImpl implements UniversityProfileService {
@@ -25,11 +30,11 @@ public class UniversityProfileServiceImpl implements UniversityProfileService {
     public University readProfile(int id) throws SQLException {
         University university = new University();
         List<University> universityList = universityDao.fetchAll();
-        for (University uniList : universityList) {
-            if (uniList.getUserId() == id) {
-                university.setUserId(uniList.getUserId());
-                university.setUniversityName(uniList.getUniversityName());
-                university.setUniversityDescription(uniList.getUniversityDescription());
+        for (int i = 0; i < universityList.size(); i++) {
+            if (universityList.get(i).getUserId() == id) {
+                university.setUserId(universityList.get(i).getUserId());
+                university.setUniversityName(universityList.get(i).getUniversityName());
+                university.setUniversityDescription(universityList.get(i).getUniversityDescription());
             }
         }
         return university;
